a1dc2a55848d61c4f4dc8ea3a365053b497c485f
Lito <lito@eordes.com>
2023-12-14 09:33:44 +0100

    Updated Slow Command with Fixed rules keys

--- app/Console/Commands/Slow.php
+++ app/Console/Commands/Slow.php
@@ -11,14 +11,26 @@ class Slow extends Command
 
     public function handle()
     {
-        $start = microtime(true);
-
         $data = $this->data();
 
-        dump('Read Data: '.sprintf('%.4f', microtime(true) - $start));
+        $this->validatorAsterisk($data);
+        $this->validatorFixed($data);
+    }
 
+    protected function data(): array
+    {
+        $start = microtime(true);
+
+        $data = ['items' => json_decode(file_get_contents(base_path('large-file.json')), true)];
+
+        dump('Read Data Time: '.sprintf('%.4f', microtime(true) - $start));
         dump('Items: '.count($data['items']));
 
+        return $data;
+    }
+
+    protected function validatorAsterisk(array $data)
+    {
         $start = microtime(true);
 
         Validator::make($data, [
@@ -29,11 +41,29 @@ class Slow extends Command
             'items.*.created_at' => ['required'],
         ]);
 
-        dump('Validator Time: '.sprintf('%.4f', microtime(true) - $start));
+        dump('validatorAsterisk Time: '.sprintf('%.4f', microtime(true) - $start));
     }
 
-    protected function data(): array
+    protected function validatorFixed(array $data)
     {
-        return ['items' => json_decode(file_get_contents(base_path('large-file.json')), true)];
+        $start = microtime(true);
+
+        $count = count($data['items']);
+        $rules = ['items' => ['array']];
+
+        for ($i = 0; $i < $count; $i++) {
+            $rules['items.'.$i.'.id'] = ['required', 'numeric'];
+            $rules['items.'.$i.'.type'] = ['required', 'string'];
+            $rules['items.'.$i.'.public'] = ['required', 'boolean'];
+            $rules['items.'.$i.'.created_at'] = ['required'];
+        }
+
+        dump('validatorFixed Prepare: '.sprintf('%.4f', microtime(true) - $start));
+
+        $start = microtime(true);
+
+        Validator::make($data, $rules);
+
+        dump('validatorFixed Time: '.sprintf('%.4f', microtime(true) - $start));
     }
 }
